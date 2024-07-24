b0454601eef167b992df67212e6821c2112f53ae
David Mkrtchyan <david.mkrtchyan@coinbase.com>
2024-07-24 13:42:04 -0700

    delete repo

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog
-
-## [1.0.0] - 2024-JUL-222
-
-### Added
-- Support for all Coinbase Advanced API REST endpoints
--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_URL: string = "api.coinbase.com";
-export const API_PREFIX: string = "/api/v3/brokerage";
-export const SERVICE_NAME: string = "retail_rest_api_proxy";
-export const ALGORITHM: string = "ES256";
