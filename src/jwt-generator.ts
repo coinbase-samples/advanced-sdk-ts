bce6c27efe68ad7a5890c947ce30f1bb0c007b40
David Mkrtchyan <david.mkrtchyan@coinbase.com>
2024-07-29 15:16:22 -0700

    add JWT_ISSUER constant

--- src/jwt-generator.ts
+++ src/jwt-generator.ts
@@ -2,12 +2,12 @@ import { BASE_URL, ALGORITHM } from "./constants";
 import * as jwt from 'jsonwebtoken';
 import * as crypto from 'crypto';
 
-
+const JWT_ISSUER = 'cdp';
 
 export function generateToken(requestMethod: string, requestPath: string): string {
     const uri = `${requestMethod} ${BASE_URL}${requestPath}`;
     const payload = {
-        iss: 'cdp',
+        iss: JWT_ISSUER,
         nbf: Math.floor(Date.now() / 1000),
         exp: Math.floor(Date.now() / 1000) + 120,
         sub: process.env.API_KEY,
