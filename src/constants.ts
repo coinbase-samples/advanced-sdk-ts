561585301821d41a3e286e730da9262ecd7b45ac
David Mkrtchyan <david.mkrtchyan@coinbase.com>
2024-08-06 14:49:23 -0700

    delete version.ts, add version to constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
-import { VERSION} from "./version";
-
 export const BASE_URL: string = "api.coinbase.com";
 export const API_PREFIX: string = "/api/v3/brokerage";
 export const ALGORITHM: string = "ES256";
+export const VERSION = "0.1.0"
 export const USER_AGENT: string = `coinbase-advanced-ts/${VERSION}`;
