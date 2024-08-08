0128abe67fea585fb9eabb14950fc2a5b1a251c0
David Mkrtchyan <david.mkrtchyan@coinbase.com>
2024-08-07 17:21:06 -0700

    add typed request object for Convert endpoints

--- src/rest/converts.ts
+++ src/rest/converts.ts
@@ -1,62 +1,49 @@
 import { API_PREFIX } from '../constants';
 import { RESTBase } from './rest-base';
+import {
+  CommitConvertTradeRequiredBodyParams,
+  CommitConvertTradeRequiredPathParams,
+  CreateConvertQuoteOptionalBodyParams,
+  CreateConvertQuoteRequiredBodyParams,
+  GetConvertTradeRequiredPathParams,
+  GetConvertTradeRequiredQueryParams,
+} from './types';
 
 export function createConvertQuote(
   this: RESTBase,
-  from_account: string,
-  to_account: string,
-  amount: string,
-  trade_incentive_metadata: object
+  requiredBody: CreateConvertQuoteRequiredBodyParams,
+  optionalBody?: CreateConvertQuoteOptionalBodyParams
 ) {
-  const bodyParams = {
-    from_account: from_account,
-    to_account: to_account,
-    amount: amount,
-    trade_incentive_metadata: trade_incentive_metadata,
-  };
-
   return this.request({
     method: 'POST',
     endpoint: `${API_PREFIX}/convert/quote`,
-    bodyParams: bodyParams,
-    isPublic: false,
-  });
-}
-
-export function commitConvertTrade(
-  this: RESTBase,
-  trade_id: string,
-  from_account: string,
-  to_account: string
-) {
-  const bodyParams = {
-    from_account: from_account,
-    to_account: to_account,
-  };
-
-  return this.request({
-    method: 'POST',
-    endpoint: `${API_PREFIX}/convert/trade/{trade_id}`,
-    bodyParams: bodyParams,
+    bodyParams: { ...requiredBody, ...optionalBody },
     isPublic: false,
   });
 }
 
 export function getConvertTrade(
   this: RESTBase,
-  trade_id: string,
-  from_account: string,
-  to_account: string
+  requiredPath: GetConvertTradeRequiredPathParams,
+  requiredQuery: GetConvertTradeRequiredQueryParams
 ) {
-  const queryParams = {
-    from_account: from_account,
-    to_account: to_account,
-  };
-
   return this.request({
     method: 'GET',
-    endpoint: `${API_PREFIX}/convert/trade/${trade_id}`,
-    queryParams: queryParams,
+    endpoint: `${API_PREFIX}/convert/trade/${requiredPath.trade_id}`,
+    queryParams: requiredQuery,
+    isPublic: false,
+  });
+}
+
+export function commitConvertTrade(
+  this: RESTBase,
+  requiredPath: CommitConvertTradeRequiredPathParams,
+  requiredBody: CommitConvertTradeRequiredBodyParams
+) {
+  return this.request({
+    method: 'POST',
+    endpoint: `${API_PREFIX}/convert/trade/${requiredPath.trade_id}`,
+    bodyParams: requiredBody,
     isPublic: false,
   });
 }
--- src/rest/types.ts
+++ src/rest/types.ts
@@ -8,12 +8,51 @@ export interface RequestOptions {
   isPublic: boolean; // Is this request public?
 }
 
+// Get Account
 export type GetAccountRequiredQueryParams = {
   account_uuid: string;
 };
 
+// List Accounts
 export type ListAccountsOptionalQueryParams = {
   limit?: number;
   cursor?: string;
   retail_portfolio_id?: string;
+};
+
+// Create Convert Quote
+export type CreateConvertQuoteRequiredBodyParams = {
+  from_account: string;
+  to_account: string;
+  amount: string;
+};
+
+export type CreateConvertQuoteOptionalBodyParams = {
+  trade_incentive_metadata?: TradeIncentiveMetadata;
+};
+
+// Get Convert Trade
+export type GetConvertTradeRequiredPathParams = {
+  trade_id: string;
+};
+
+export type GetConvertTradeRequiredQueryParams = {
+  from_account: string;
+  to_account: string;
+};
+
+// Commit Convert Trade
+export type CommitConvertTradeRequiredPathParams = {
+  trade_id: string;
+};
+
+export type CommitConvertTradeRequiredBodyParams = {
+  from_account: string;
+  to_account: string;
+};
+
+// Misc.
+export type TradeIncentiveMetadata = {
+  user_incentive_id: string;
+  code_val: string;
 };
