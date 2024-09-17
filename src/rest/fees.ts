8e7c426c5f18c4e311f42729baa68e4f44cd068e
David Mkrtchyan <david.mkrtchyan@coinbase.com>
2024-09-17 14:56:26 -0700

    fix Get Transaction Summary method bug

--- src/rest/fees.ts
+++ src/rest/fees.ts
@@ -6,14 +6,14 @@ import {
 } from './types/fees-types';
 import { request } from './types/request-types';
 
-// [POST] Get Transaction Summary
+// [GET] Get Transaction Summary
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_commitconverttrade
 export function getTransactionSummary(
   this: RESTBase,
   requestParams: GetTransactionsSummaryRequest
 ): Promise<GetTransactionsSummaryResponse> {
   return this.request({
-    method: request.POST,
+    method: request.GET,
     endpoint: `${API_PREFIX}/transaction_summary`,
     queryParams: requestParams,
     isPublic: false,
